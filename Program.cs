59ed3262531121e7d119aba37eb45097920bbbf8
BaurzhanOrozali <123200750+BaurzhanOrozali@users.noreply.github.com>
2023-02-04 00:59:03 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿// Домашняя работа по семинару 1
+
+
+// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+/*
+Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (num1 > num2)
+{
+    Console.WriteLine("большая цифра " + num1 + " меньшая цифра " + num2 );
+}
+else
+{
+    Console.WriteLine("большая цифра " + num2 + " меньшая цифра " + num1 );
+}
+*/
+//------------------------------------------------------------
+
+
+ //Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.Write("Введите первое число ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 > num2)
+// {
+//     if(num1 > num3)
+//     {
+//         Console.WriteLine($"максимальное значение {num1}");
+//     }
+//      else
+//     {
+//          Console.WriteLine($"максимальное значение {num3}");
+//     }
+// }
+// else
+// {
+//     if(num2> num3)
+//     {
+//         Console.WriteLine($"максимальное значение {num2}");
+//     }
+//      else
+//     {
+//          Console.WriteLine($"максимальное значение {num3}");
+//     }
+// }
+
+//-----------------------------------------------------
+
+
+// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+// Console.Write("Input an integer number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num%2 == 0)
+// {
+//     Console.WriteLine( "число четное " + num);
+// }
+// else
+// {
+//     Console.WriteLine( "число нечетное " + num);
+// }
+
+//--------------------------------------------------
+
+//Задача 4:  Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+/*
+Console.Write("введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+
+while (current <= num)
+{
+    current += 1;
+    if (current%2 == 0)
+    {
+        Console.Write(current + " ");
+    }
+}
+*/
+
+ 
+
+
+
+
+
+
